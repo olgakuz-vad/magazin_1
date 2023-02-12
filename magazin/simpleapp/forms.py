8f98188a7e99a0294bbb91f0c2d56d3ad66fe392
olgakuz-vad <107033932+olgakuz-vad@users.noreply.github.com>
2023-02-12 20:52:31 +0300

    First fiel

---
+++ magazin/simpleapp/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    description = forms.CharField(min_length=20)
+
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'description',
+            'quantity',
+            'category',
+            'price',
+        ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get("name")
+        description = cleaned_data.get("description")
+
+        if name == description:
+            raise ValidationError(
+                "Описание не должно быть идентично названию."
+            )
+
+        return cleaned_data
